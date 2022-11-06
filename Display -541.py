42af773fa2b272665122e6a05bf54d3b1b35272b
gdwin12 <117602685+gdwin12@users.noreply.github.com>
2022-11-06 23:22:40 +0100

    Add files via upload

---
+++ Display -541.py
@@ -0,0 +1,5 @@
+Name = input("Enter your Name: ")
+Age = input("Enter Your Age: ")
+Department = input("Enter Your Department: ")
+
+print("Your name is "+ Name +".", "You are "+ Age + " years old.", "You are in "+ Department + " department." )
